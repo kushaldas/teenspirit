6e1ec13faea381424f1c492a58c636cb303fd563
Kushal Das <kushaldas@gmail.com>
2014-07-22 18:01:26 +0530

    Initial code which can save a log file.

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from werkzeug.wsgi import SharedDataMiddleware
+
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['log', 'txt'])
+
+APP = Flask(__name__)
+APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+APP.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+APP.wsgi_app = SharedDataMiddleware(APP.wsgi_app, {
+    '/uploads':  APP.config['UPLOAD_FOLDER']
+})
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@APP.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(APP.config['UPLOAD_FOLDER'], filename))
+            return "Log uploaded."
+
+    return '''
+    <!doctype html>
+    <head>
+        <title>Upload new File</title>
+    </head>
+    <body>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
